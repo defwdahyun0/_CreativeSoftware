732250df46692327f5c2c651df5f7be9bba1848b
Dahyun Woo <wdh112139@ajou.ac.kr>
2020-05-11 11:40:16 +0900

    print - function - flush 아직도 정확히 어떻게 작동하는지 모르겠음(변기가 자꾸 생각남)

---
+++ print_lab.py
@@ -0,0 +1,21 @@
+import sys
+filep = open("output.txt", "w") #"rw" cannot be use
+a = 100
+b = "String_B"
+print("----------Sep----------")
+print("Deafult   - ", a, b)
+print("Sep=$$$   - ", a, b, sep='$$$')
+print("Sep=\_n   - ", a, b, sep='\n')
+print("Sep=''    - ", a, b, sep='')
+print("----------End----------")
+print("End=      - ", a, b, end='')
+print("Next Line", end='')
+print("----------End----------")
+print("End=      - ", a, b, end='=====\n')
+print("Next Line", end='=====\n')
+print("----------End--FILE--------")
+print("End=      - ", a, b, end='=====\n')
+print("  sys.stdin - type ", type(sys.stdout))
+print("Next Line", end='=====\n', file=filep)
+print("Next Line", end='=====\n', file=filep, flsuh=True) # We don't know how to use it
+filep.close()
