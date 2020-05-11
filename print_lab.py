acde89507177dcc925aad77e6d701f8fb1a16790
Dahyun Woo <wdh112139@ajou.ac.kr>
2020-05-11 11:44:09 +0900

    Update print_lab.py

--- print_lab.py
+++ print_lab.py
@@ -5,7 +5,7 @@ b = "String_B"
 print("----------Sep----------")
 print("Deafult   - ", a, b)
 print("Sep=$$$   - ", a, b, sep='$$$')
-print("Sep=\_n   - ", a, b, sep='\n')
+print("Sep=\_n   - ", a, b, sep='\n') ## 앞에 \n을 그냥쓰면 거기 줄바꿈 
 print("Sep=''    - ", a, b, sep='')
 print("----------End----------")
 print("End=      - ", a, b, end='')
@@ -15,7 +15,7 @@ print("End=      - ", a, b, end='=====\n')
 print("Next Line", end='=====\n')
 print("----------End--FILE--------")
 print("End=      - ", a, b, end='=====\n')
-print("  sys.stdin - type ", type(sys.stdout))
+print("   sys.stdin - type ", type(sys.stdout))
 print("Next Line", end='=====\n', file=filep)
 print("Next Line", end='=====\n', file=filep, flsuh=True) # We don't know how to use it
 filep.close()
